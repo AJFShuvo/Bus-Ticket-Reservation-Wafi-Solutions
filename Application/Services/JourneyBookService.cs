6cf059ed9b68a9c370f824c089e16649e7f6bf6c
AJFShuvo <ajfshuvo98@gmail.com>
2025-10-26 15:10:03 +0600

    add book services

---
+++ Application/Services/JourneyBookService.cs
@@ -0,0 +1,34 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class JourneyBookService 
+    {
+        private readonly IJourneyBookRepository _repository;
+
+        public JourneyBookService(IJourneyBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> BookSeatAsync(BookSeatRequestDto request)
+        {
+            var booking = new Bookings
+            {
+                JourneyId = request.JourneyId,
+                SeatNumber = request.SeatNumber,
+                BookedByUser = request.UserEmail
+            };
+
+            var result = await _repository.BookSeatAsync(booking);
+            return result != null;
+        }
+    }
+}
